574f5f1cdad12db796348bc8feb16a01b78d9fa1
Fengda HUANG <h@phodal.com>
2015-11-20 13:39:03 +0800

    Add basic search logic

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,31 +1,37 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
 
-})
+  })
 
-.controller('ArticleListsCtrl', function($scope, Blog) {
-  $scope.articles = null;
-  $scope.blogOffset = 0;
+  .controller('ArticleListsCtrl', function ($scope, Blog) {
+    $scope.articles = null;
+    $scope.blogOffset = 0;
 
-  $scope.doRefresh = function () {
+    $scope.doRefresh = function () {
+      Blog.async('http://deploy.baimizhou.net/api/blog/articles.json').then(function (results) {
+        $scope.articles = results;
+      });
+      $scope.$broadcast('scroll.refreshComplete');
+      $scope.$apply()
+    };
+
     Blog.async('http://deploy.baimizhou.net/api/blog/articles.json').then(function (results) {
       $scope.articles = results;
     });
-    $scope.$broadcast('scroll.refreshComplete');
-    $scope.$apply()
-  };
+  })
 
-  Blog.async('http://deploy.baimizhou.net/api/blog/articles.json').then(function (results) {
-    $scope.articles = results;
+
+  .controller('SearchCtrl', function ($scope, $stateParams, $sanitize, $sce, Blog) {
+    $scope.query = "";
+  })
+
+
+  .controller('ArticleCtrl', function ($scope, $stateParams, $sanitize, $sce, Blog) {
+    $scope.article = {};
+    Blog.async('http://deploy.baimizhou.net/api/' + $stateParams.slug + '.json').then(function (results) {
+      $scope.article = results;
+      $scope.htmlContent = $sce.trustAsHtml($scope.article.articleHTML);
+    });
+
   });
-})
-
-.controller('ArticleCtrl', function($scope, $stateParams, $sanitize, $sce, Blog) {
-  $scope.article = {};
-  Blog.async('http://deploy.baimizhou.net/api/' + $stateParams.slug + '.json').then(function (results) {
-    $scope.article = results;
-    $scope.htmlContent = $sce.trustAsHtml($scope.article.articleHTML);
-  });
-
-});
