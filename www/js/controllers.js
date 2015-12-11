949332955037643fa56a83d0812c8c49152ca091
Fengda HUANG <h@phodal.com>
2015-12-11 22:56:02 +0800

    remove login logic

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,7 @@
 angular.module('starter.controllers', [])
 
-  .controller('AppCtrl', function ($scope, $ionicModal, $http, $timeout, $cordovaToast, $localstorage) {
-    $scope.loginData = {};
-    $scope.noLogin = true;
-    if ($localstorage.get('username')) {
-      $scope.loginData.username = $localstorage.get('username');
-    }
+  .controller('AppCtrl', function () {
 
-    $ionicModal.fromTemplateUrl('templates/login.html', {
-      scope: $scope
-    }).then(function (modal) {
-      $scope.modal = modal;
-    });
-
-    $scope.closeLogin = function () {
-      $scope.modal.hide();
-    };
-
-    $scope.login = function () {
-      $scope.modal.show();
-    };
-
-    $scope.logout = function () {
-      $scope.noLogin = true;
-      $localstorage.remove('token');
-    };
-
-    $scope.doLogin = function () {
-      var username = $scope.loginData.username;
-      var password = $scope.loginData.password;
-
-      var github = new Github({
-        username: username,
-        password: password,
-        auth: "basic"
-      });
-      var user = github.getUser();
-      user.notifications(function (err) {
-        if (err === null) {
-          $scope.noLogin = false;
-          $localstorage.set('username', username);
-          //$cordovaToast
-          //  .show('Login Success', 'long', 'center')
-          //  .then(function (success) {
-          //    $scope.closeLogin();
-          //  }, function (error) {
-          //    // error
-          //  });
-          $scope.closeLogin();
-        }
-      });
-    };
   })
 
 
